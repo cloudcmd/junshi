86adde4a7e91f29c1d17a27a20fd300e3e63e138
coderaiser <mnemonic.enemy@gmail.com>
2014-06-27 01:13:41 -0700

    feature(junshi) add

--- web/AppDelegate.h
+++ web/AppDelegate.h
@@ -4,12 +4,15 @@
 //
 //  Created by user on 6/26/14.
 //  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
-//
+///Users/user/Documents/web/web/AppDelegate.m
 
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSButton *Connect;
+@property (weak) IBOutlet NSButton *Close;
+@property (weak) IBOutlet NSTextField *Edit;
 
 @end
--- web/AppDelegate.m
+++ web/AppDelegate.m
@@ -9,10 +9,24 @@
 #import "AppDelegate.h"
 
 @implementation AppDelegate
+- (IBAction)Connect:(id)sender{
+    NSString *url = [_Edit stringValue];
+    
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setMessageText:url];
+    [alert runModal];
 
+}
+- (IBAction)Close:(id)sender {
+    [NSApp terminate:self];
+}
+	
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+        // Insert code here to initialize your application
+}
+
+- (IBAction)Edit:(id)sender {
 }
 
 @end
